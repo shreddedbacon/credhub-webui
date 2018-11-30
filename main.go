0fcf5e7ef0454d677e56932fa83603655cc38a41
Ben Jackson <b@benjackson.email>
2018-11-30 14:45:32 +1100

    tested auth works, tidy up soon

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 	"net/http"
 	"net/url"
 	"strings"
+	"os"
+	"bytes"
+	"encoding/json"
+	"time"
 )
 
 type ClientStruct struct {
@@ -16,19 +20,25 @@ type ClientStruct struct {
 	ClientSecret string
 }
 
-func keepLines(s string, n int) string {
-	result := strings.Join(strings.Split(s, "\n")[:n], "\n")
-	return strings.Replace(result, "\r", "", -1)
+type AuthResponse struct {
+	AccessToken     string `json:"access_token"`
+	TokenType string `json:"token_type"`
+	Scope string `json:"scope"`
+	Error string `json:"error"`
+	ErrorDesc string `json:"error_description"`
 }
 
 var (
 	// key must be 16, 24 or 32 bytes long (AES-128, AES-192 or AES-256)
-	key   = []byte("super-secret-key")
-	store = sessions.NewCookieStore(key)
+	key         = []byte("super-secret-key")
+	store       = sessions.NewCookieStore(key)
+	auth_server1 = os.Getenv("AUTH_SERVER1")
+	auth_server2 = os.Getenv("AUTH_SERVER2")
+	cookie_name = "auth-cookie"
 )
 
 func secret(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
+	session, _ := store.Get(r, cookie_name)
 
 	// Check if user is authenticated
 	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
@@ -36,30 +46,31 @@ func secret(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	var netClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
+	req, _ := http.NewRequest("GET", auth_server1+"/api/v1/data?name-like=", bytes.NewBuffer([]byte("")))
+	access_token := session.Values["access_token"].(string)
+	req.Header.Add("authorization", "bearer "+access_token)
+	req.Header.Set("Content-Type", "application/json")
+	resp, _ := netClient.Do(req)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Println("post:\n", string(body))
 	// Print secret message
-	fmt.Fprintln(w, "The cake is a lie!")
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
-
-	// Authentication goes here
-	// ...
-
-	// Set user as authenticated
-	session.Values["authenticated"] = true
-	session.Save(r, w)
+	fmt.Fprintln(w, string(body))
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "cookie-name")
+	session, _ := store.Get(r, cookie_name)
 
 	// Revoke users authentication
 	session.Values["authenticated"] = false
+	session.Values["access_token"] = ""
 	session.Save(r, w)
 }
 
-func root(w http.ResponseWriter, r *http.Request) {
+func login(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("login.html"))
 	if r.Method != http.MethodPost {
 		tmpl.Execute(w, nil)
@@ -73,41 +84,50 @@ func root(w http.ResponseWriter, r *http.Request) {
 	_ = details
 	fmt.Println(details)
 
-	// get auth url
-	auth_server := "http://google.com"
-	resp, err := http.Get(auth_server)
+	// get auth url (this needs to be fixed up a bit because its not really even being used yet to extract the url)
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true} //ignore cert for now
+	resp, err := http.Get(auth_server1 + "/info")
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Println("get:\n", keepLines(string(body), 3))
+	fmt.Println("get:\n", string(body))
 
 	// post auth request
-	auth_url := "https://google.com"
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true} //ignore cert for now
-	resp, err = http.PostForm(auth_url, url.Values{"q": {"github"}})
+	resp, err = http.PostForm(auth_server2+"/oauth/token", url.Values{
+		"client_id": {details.ClientID},
+		"client_secret": {details.ClientSecret},
+		"grant_type": {"client_credentials"},
+		"response_type": {"token"},
+	})
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
 	body, err = ioutil.ReadAll(resp.Body)
-	fmt.Println("post:\n", keepLines(string(body), 3))
-
-	session, _ := store.Get(r, "cookie-name")
+	fmt.Println("post:\n", string(body))
+	textBytes := []byte(body)
+	list := AuthResponse{}
+  if err := json.Unmarshal([]byte(textBytes), &list); err != nil {
+    fmt.Println(err)
+  }
+	fmt.Println(list)
+	session, _ := store.Get(r, cookie_name)
 	// Authentication goes here
 	// ...
 	// Set user as authenticated
 	session.Values["authenticated"] = true
+	session.Values["access_token"] = list.AccessToken
 	session.Save(r, w)
 
 	tmpl.Execute(w, struct{ Success bool }{true})
 }
 
 func main() {
-	http.HandleFunc("/", root)
+	http.HandleFunc("/", login)
 	http.HandleFunc("/secret", secret)
-	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
 
 	http.ListenAndServe(":8080", nil)
